e80da4faef8b662c986f76ed2de4bd6d9a688a52
Galina Atamankina <g.atamankina@win.tu-berlin.de>
2017-06-08 00:04:39 +0200

    Implemented error handling, json schema test. Added scenarios to main.

--- src/com/example/tests/Main.java
+++ src/com/example/tests/Main.java
@@ -1,5 +1,6 @@
 /**
- * 
+ * Running tests for RESTApi.
+ * The tested scenarios and results are shown in the console.
  */
 package com.example.tests;
 
@@ -19,6 +20,7 @@ public class Main {
 	 * @throws ClientProtocolException 
 	 */
 	public static void main(String[] args) throws ClientProtocolException, IOException {
+		//Input
 		String correct_url = 
 				"https://weather.cit.api.here.com/weather/1.0/report.json?product=observation&name=Barcelona&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg";
 		String url_noproduct = 
@@ -29,12 +31,45 @@ public class Main {
 				"https://weather.cit.api.here.com/weather/1.0/report.json?product=observation&name=Barcelona&app_code=AJKnXv84fjrb0KIHawS0Tg";
 		String url_noappcode = 
 				"https://weather.cit.api.here.com/weather/1.0/report.json?product=observation&name=Barcelona&app_id=DemoAppId01082013GAL";
+		String url_othermime = "https://www.google.com/";
+		String schema_classpath = "weather_schema.json";
+		String wrong_schema = "wrong_schema.json";
 		
+		//Tests
 		RestAPITest rt = new RestAPITest();
+		System.out.println("Verify response status code 200...");
+		System.out.println("Test result for the correct request: " + correct_url);
+		rt.verifyCorrectRequest(correct_url);
+		System.out.println("Test result for the incorrect request: " + url_nocity);
+		rt.verifyCorrectRequest(url_nocity);
 		
-		rt.testCorrectRequest(correct_url);
-		rt.testCorrectRequest(url_noproduct);
+		System.out.println("Verify response status code 400...");
+		System.out.println("Test result for the correct request:");
+		rt.verifyBadRequest(correct_url);
+		System.out.println("Test result for the incorrect request:");
+		rt.verifyBadRequest(url_nocity);
+		
+		System.out.println("Verify response media type...");
+		System.out.println("Test result for the matching media type:");
+		rt.verifyMediaTypeJson(correct_url);
+		System.out.println("Test result for the not matching media type:");
+		rt.verifyMediaTypeJson(url_othermime);
+		
+		System.out.println("Verify expected values for specific attributes...");
+		System.out.println("Test results for correct values:");
+		rt.verifyResponseFieldValue(correct_url, "/observations/location", "city", "Barcelona");
+		rt.verifyResponseFieldValue(correct_url, "/observations/location", "country", "Spain");
+		System.out.println("Test results for incorrect values:");
 		rt.verifyResponseFieldValue(correct_url, "/observations/location", "city", "Berlin");
+		rt.verifyResponseFieldValue(correct_url, "/observations/location", "country", "Germany");
+		
+		System.out.println("Verify JSON schema of the response...");
+		System.out.println("Test results for the correct JSON schema:");
+		rt.verifyResponseJsonSchema(correct_url, schema_classpath);
+		System.out.println("Test results for the incorrect JSON schema:");
+		rt.verifyResponseJsonSchema(correct_url, wrong_schema);
+		
+		System.out.println("Tests done.");
 	
 	
 	}
--- src/com/example/tests/restapiTest.java
+++ src/com/example/tests/restapiTest.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * Different tests for RESTApi.
  */
 package com.example.tests;
 
@@ -16,8 +16,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.restassured.response.Response;
 import static com.jayway.restassured.RestAssured.get;
 import static org.junit.Assert.*;
-
-
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * @author Galina
@@ -26,81 +26,125 @@ import static org.junit.Assert.*;
 public class RestAPITest {
 	
 	/**
-	 * @param url
+	 * Test for expected HTTP code of the response. This one tests 200.
+	 * For testing other codes, copy-paste and code adjustment in the assert clause is required.
+	 * Test passes for responses with the code 200 (OK) and fails for other.
+	 * @param url = request URL
 	 * @throws ClientProtocolException
 	 * @throws IOException
 	 */
-	public void testCorrectRequest(String url) throws ClientProtocolException, IOException{
+	public void verifyCorrectRequest(String url) throws ClientProtocolException, IOException{
+		
 		HttpUriRequest request = new HttpGet(url);
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+		
 		try{
 		assertEquals(
 			     response.getStatusLine().getStatusCode(),
 			     HttpStatus.SC_OK);
-		}catch(AssertionError ae){System.out.println("Status code is NOT 200 as expected.");}
+		}catch(AssertionError ae)
+				{	System.err.println("Status code is NOT 200 as expected.");
+					ae.printStackTrace();
+					return;}
+		
 		System.out.println("Status code is 200 as expected.");
-		System.out.println(response.toString());
 		
 	}
 
 	/**
-	 * @param url
+	 * Test for expected HTTP code of the response. This one tests 400.
+	 * Test passes for responses with the code 400 (bad request) and fails for other.
+	 * @param url = request URL
 	 * @throws ClientProtocolException
 	 * @throws IOException
 	 */
-	public void testBadRequest(String url) throws ClientProtocolException, IOException{
+	public void verifyBadRequest(String url) throws ClientProtocolException, IOException{
 		HttpUriRequest request = new HttpGet(url);
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+		
+		try{
 		assertEquals(
 			     response.getStatusLine().getStatusCode(),
 			     HttpStatus.SC_BAD_REQUEST);
+		}catch(AssertionError ae)
+		{	System.err.println("Status code is NOT 400 as expected.");
+			ae.printStackTrace();
+			return;}
 		
+		System.out.println("Status code is 400 as expected.");
 		
 	}
 	
 	
 	/**
-	 * @param url
+	 * Test for verifying expected media type. This one passes for application/json.
+	 * To check other media types, copy-paste and adjust the mime type variable.
+	 * @param url = request URL
 	 * @throws ClientProtocolException
 	 * @throws IOException
 	 */
 	public void verifyMediaTypeJson(String url) throws ClientProtocolException, IOException{
 		String jsonMimeType = "application/json";
 		HttpUriRequest request = new HttpGet(url);
-		 
-		HttpResponse response = HttpClientBuilder.create().build().execute(request);
-		 
-		String mimeType = ContentType.getOrDefault(response.getEntity()).getMimeType();
-		assertEquals(jsonMimeType, mimeType );
 		
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+		String mimeType = ContentType.getOrDefault(response.getEntity()).getMimeType();
+		
+		try{
+		assertEquals(jsonMimeType, mimeType );
+		}catch(AssertionError ae)
+		{	System.err.println("Media type is NOT application/json as expected.");
+			ae.printStackTrace();
+			return;}
+		
+		System.out.println("Media type is application/json as expected.");
 	}
 	
 	/**
-	 * @param url
-	 * @param path
-	 * @param key
-	 * @param expected
+	 * Test to check values of specific fields in the response.
+	 * It searches for the given key under the given path and compares the values 
+	 * (in this implementation only matches String to String).
+	 * @param url = request URL
+	 * @param path = path to the "key" in JSON response
+	 * @param key = key in JSON response
+	 * @param expected = expected value of the "key" in JSON response
 	 * @throws IOException
 	 */
 	public void verifyResponseFieldValue(String url, String path, String key, String expected) throws IOException{
+		//using rest-assure library
 		Response response = get(url);
-		System.out.println(response.asString());
-		
+
 		ObjectMapper mapper = new ObjectMapper();
+		//using jackson library
 		JsonNode node = mapper.readTree(response.asString());
-		
 		String actual = node.at(path).findValue(key).asText();
-		System.out.println(actual);
 		
+		try{
 		assertEquals(expected, actual);
-			
+		}catch(AssertionError ae)
+		{	System.err.println("The value is not as expected for the key: " + key);
+			ae.printStackTrace();
+			return;}
+		
+		System.out.println("The value is as expected for the key: " + key);	
 	}
 	
 	/**
-	 * @param url
+	 * Test to verify if the response schema matches the expected JSON schema
+	 * @param url = request URL
 	 * @param classpath
 	 */
-	public void validateResponseJsonSchema(String url, String classpath){
+	public void verifyResponseJsonSchema(String url, String classpath){
+		//using rest-assure library
+		Response response = get(url);		 
 		
+		try{
+		assertThat(response.asString(), matchesJsonSchemaInClasspath(classpath));
+		}catch(AssertionError ae)
+		{	System.err.println("The response schema doesn't match the expected schema.");
+			ae.printStackTrace();
+			return;}
+		
+		System.out.println("The response schema matches the expected schema.");
 	}
 }
